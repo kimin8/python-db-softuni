173bf1c7023bc1e0465ce305331c3020f9e0ccd0
Kimi Nikolov <126898516+kimin8@users.noreply.github.com>
2024-06-12 16:06:10 +0300

    Data Aggregation Exercise

---
+++ data-aggregation-exercise/19.sql
@@ -0,0 +1,7 @@
+CREATE TABLE employees_projects(
+    id INTEGER PRIMARY KEY,
+    employee_id INTEGER,
+    project_id INTEGER,
+    FOREIGN KEY (employee_id) REFERENCES employees(id),
+    FOREIGN KEY (project_id) REFERENCES projects(id)
+);
