dfabd4a03d1a029a2246091364bdeb92777d564d
Kimi Nikolov <126898516+kimin8@users.noreply.github.com>
2024-07-04 19:12:51 +0300

    Initial Commit

---
+++ PostgreSQL/basic-crud-exercise/25.sql
@@ -0,0 +1,2 @@
+SELECT "name", "start_date" FROM projects
+WHERE "name" LIKE 'MOUNT%' ORDER BY "id";
---
+++ PostgreSQL/data-aggregation-exercise/20.sql
@@ -0,0 +1,1 @@
+SELECT * FROM departments JOIN employees ON departments.id = employees.department_id;
