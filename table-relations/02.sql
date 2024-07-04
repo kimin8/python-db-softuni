8648cc202fb140cbe99d7dcc8ce97da5584b28f3
Kimi Nikolov <126898516+kimin8@users.noreply.github.com>
2024-07-04 19:10:26 +0300

    Delete table-relations directory

--- table-relations/02.sql
+++
@@ -1,7 +0,0 @@
-SELECT
-    v.driver_id,
-    v.vehicle_type,
-    CONCAT(c.first_name, ' ', c.last_name)
-FROM campers AS c
-JOIN vehicles AS v
-    ON c.id = v.driver_id;
--- table-relations/03.sql
+++
@@ -1,8 +0,0 @@
-SELECT
-    r.start_point,
-    r.end_point,
-    r.leader_id,
-    CONCAT(c.first_name, ' ', c.last_name)
-FROM routes AS r
-JOIN campers AS c
-    ON r.leader_id = c.id
