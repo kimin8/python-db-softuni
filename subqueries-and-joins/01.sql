0d9f7a2b80b4663be9420c6817bc242ba21dfd3d
Kimi Nikolov <126898516+kimin8@users.noreply.github.com>
2024-07-04 19:10:12 +0300

    Delete subqueries-and-joins directory

--- subqueries-and-joins/01.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-    t.town_id AS town_id,
-    t.name AS town_name,
-    a.address_text AS address_text
-FROM towns AS t
-    JOIN addresses AS a
-        ON t.town_id = a.town_id
-WHERE name IN ('Sofia', 'San Francisco', 'Carnation')
-ORDER BY town_id, address_id;
