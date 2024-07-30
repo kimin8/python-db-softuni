0e598048146f20f5e389a74b624d04ec8312b60e
Kimi Nikolov <126898516+kimin8@users.noreply.github.com>
2024-07-30 22:44:18 +0300

    Advanced Queries In Django

---
+++ PythonORM/AdvancedQueriesInDjango/main_app/managers.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.db.models import QuerySet
+
+
+class ProductManager(models.Manager):
+    def available_products(self) -> QuerySet:
+        return self.filter(is_available=True)
+
+    def available_products_in_category(self, category_name: str) -> QuerySet:
+        return self.filter(category__name=category_name, is_available=True)
